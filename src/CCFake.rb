6a1f58cbe260254b626bafd23014256173a75c1f
Chris Socha <cschlumb@thoughtworks.com>
2014-03-11 16:01:23 +0000

    Added basic auth to all routes

--- src/CCFake.rb
+++ src/CCFake.rb
@@ -2,6 +2,10 @@ require 'sinatra'
 
 File.open('sinatra.pid', 'w') {|f| f.write Process.pid }
 
+use Rack::Auth::Basic, "Restricted Area" do |username, password|
+  username == 'user' and password == 'pass'
+end
+
 get '/' do
   send_file 'cctray.xml'
 end
